4a49e8b6eb31e2a583689caf05b5d0e8e5f46a0f
Sujan Shrestha <raju40rr@gmail.com>
2025-12-02 21:45:09 -0600

    designing the UI

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,12 @@
 export const Home = () => {
   return (
-    <div> Home Page  </div>
+    <div className="min-h-screen"> 
+      
+      {/* {Theme Toggle} */}
+      {/* {Background Effects} */}
+      {/* {NavBar} */}
+      {/* {Main Content} */}
+      {/* {Footer } */}
+       </div>
   )
 }
